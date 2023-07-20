18feae028560a0095545114e2d5ba9274161a8cc
cking150 <135468387+cking150@users.noreply.github.com>
2023-07-19 22:44:00 -0400

    Update SmartRecipe.py

--- SmartRecipe.py
+++ SmartRecipe.py
@@ -1,70 +1,28 @@
 import random
-import os
+import requests
+from bs4 import BeautifulSoup
 
-def read_recipe(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            return file.readlines()
-    except FileNotFoundError:
-        print(f"Error: The file '{file_path}' was not found.")
-        return []
+def get_recipe_from_website(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-def generate_recipe(title, ingredients, instructions):
-    recipe = f"\n{title}\n\nIngredients:\n"
-    random.shuffle(ingredients)
-    for ingredient in ingredients:
-        recipe += f"- {ingredient.strip()}\n"
+        # Extracting ingredients and instructions from the website
+        ingredients = [ingredient.get_text().strip() for ingredient in soup.select('.recipe-ingred_txt')]
+        instructions = [step.get_text().strip() for step in soup.select('.recipe-directions__list--item')]
+        return ingredients, instructions
+    else:
+        print("Failed to fetch the recipe from the website.")
+        return [], []
 
-    recipe += "\nInstructions:\n"
-    random.shuffle(instructions)
-    for idx, instruction in enumerate(instructions, start=1):
-        recipe += f"{idx}. {instruction.strip()}\n"
-
-    return recipe
+# ... (rest of the code remains the same)
 
 def main():
-    recipe_files = [
-        "Users/kingcharles/banana bread.txt",
-        "Users/kingcharles/black bean chili.txt",
-        "Users/kingcharles/Pasta Salad.txt",
-    ]
+    recipe_url = "https://www.allrecipes.com/recipe/222093/healthier-slow-cooker-beef-stew-i/"
+    all_ingredients, all_instructions = get_recipe_from_website(recipe_url)
 
-    # Modify the file paths to be absolute paths using os.path.abspath()
-    home_directory = os.path.expanduser("~")
-    recipe_files = [os.path.abspath(os.path.join(home_directory, file_path)) for file_path in recipe_files]
-
-    all_ingredients = []
-    all_instructions = []
-
-    for file_path in recipe_files:
-        ingredients, instructions = read_recipe(file_path)
-
-        # Debugging statements
-        print(f"File: {file_path}")
-        print("Ingredients:", ingredients)
-        print("Instructions:", instructions)
-
-        all_ingredients.extend(ingredients)
-        all_instructions.extend(instructions)
-
-    while True:
-        print("Menu:")
-        print("1. Generate Smart Recipe")
-        print("2. Quit")
-
-        choice = input("Enter your choice (1/2): ")
-
-        if choice == '1':
-            title = "Smart Recipe"
-            random_recipe = generate_recipe(title, all_ingredients, all_instructions)
-            print(random_recipe)
-        elif choice == '2':
-            print("Goodbye!")
-            break
-        else:
-            print("Invalid choice. Please select a valid option.")
+    # ... (rest of the code remains the same)
 
 if __name__ == "__main__":
     main()
-
 
